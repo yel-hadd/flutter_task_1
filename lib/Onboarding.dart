6af3146bde33264af531cf113c2cec55cb14af61
Yassine El Haddad <megahaddad15@gmail.com>
2023-02-13 22:28:49 +0100

    latest updates

--- lib/Onboarding.dart
+++ lib/Onboarding.dart
@@ -15,18 +15,40 @@ class _OnboardingState extends State<Onboarding> {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image : AssetImage("assets/onboarding_bg.jpg"),
+            image: AssetImage("assets/onboarding_bg.jpg"),
             fit: BoxFit.cover,
           ),
         ),
         child: Center(
-          child : Column(
+          child: Column(
             children: <Widget>[
+              SizedBox(height: 500),
               SizedBox(height: 90, child: Image.asset("assets/icon.png")),
-          ],
+              SizedBox(height: 40),
+              Text(
+                'Welcome to our store',                  
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+                
+              ),
+              Text(
+                'Get your groceries as fast as one hour',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Center(
+                child:
+                  MaterialButton(
+                    onPressed: () {},
+                    child:
+                      Text("Get Started"),
+                  )
+              ),
+            ],
+          ),
         ),
-        ),
-        
       ),
     );
   }
